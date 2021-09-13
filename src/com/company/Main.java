4c4a59c4e9aaea1b889f6de6406e45b0616268c0
salta <saltanov.anton@yandex.ru>
2021-09-13 22:20:34 +0900

    Commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String str1 = scan.next();
-        char op = scan.next().charAt(0);
-        String str2 = scan.next();
-        int num1 = Integer.parseInt(str1, 10);
-        int num2 = Integer.parseInt(str2, 10);
+        if (scan.hasNextInt()) {
+            int num1 = scan.nextInt();
+            char op = scan.next().charAt(0);
+            int num2 = scan.nextInt();
             if ((num1 <= 10 && num2 <= 10) && (num1 >= -10 && num2 >= -10)) {
                 switch (op) {
                     case '+':
@@ -24,14 +23,54 @@ public class Main {
                     case '*':
                         System.out.println(num1 * num2);
                 }
-            } else {
-                System.out.println("Неверное число");
             }
+        }
+        if (scan.hasNext()) {
+            String str1 = scan.next();
+            char op = scan.next().charAt(0);
+            String str2 = scan.next();
+            int numRom1 = rom(str1);
+            int numRom2 = rom(str2);
+            int result = 0;
+            if ((numRom1 > 0 && numRom2 > 0) && (numRom1 <= 10 && numRom2 <= 10)) {
+                switch (op) {
+                    case '+':
+                        result = numRom1 + numRom2;
+                        break;
+                    case '-':
+                        result = numRom1 - numRom2;
+                        break;
+                    case '/':
+                        result = numRom1 / numRom2;
+                        break;
+                    case '*':
+                        result = numRom1 * numRom2;
+                }
+            } String res1 = toArab(result);
+            System.out.println(res1);
+        }
     }
-    static int parseInt(String str1, String str2) {
-        int num1 = Integer.parseInt(str1);
-        int num2 = Integer.parseInt(str2);
-            return num1 + num2;
+
+    private static int rom(String str) {
+        String[] rum = {"I", "II", "III", "IV", "V", "VI", "XII", "VIII", "IX", "X"};
+        int num1 = 0;
+        for (int i = 0; i < 10; i++)
+            if (rum[i].equals(str)) {
+                num1 = num1 + i + 1;
+            }
+        return num1;
+    }
+    public static String toArab (int number) {
+        int[] arabArray = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanArray = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < arabArray.length; i += 1) {
+            while (number >= arabArray[i]) {
+                number -= arabArray[i];
+                res.append(romanArray[i]);
+            }
+        }
+        return res.toString();
     }
 
 }
